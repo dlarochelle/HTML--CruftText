457ae9913b4048ea1d191c01f5d80bfd96fd9b51
Linas Valiukas <shirshegsm@gmail.com>
2014-01-18 03:55:17 +0200

    Modify the POD of the module (guessing from the original unit test, the module doesn't leave the <!-- comment text --> but removes it as well)

--- lib/HTML/CruftText.pm
+++ lib/HTML/CruftText.pm
@@ -267,9 +267,7 @@ The following tactics are used to remove cruft text:
 * clickprint markers -- many web sites have clickprint annotation comments that
   explicitly mark whether text should be included.
 
-* Removal of HTML tags in comments: we remove any HTML tags within <!-- -->
-  comments but keep other comment text. This makes the result easier to process
-  with regular expressions.
+* Removal of HTML tags in comments.
 
 * Close tags that span multiple lines within an single open tag. For example,
   we would change:
